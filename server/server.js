ff39a6312c7a0c7df902007747c1664a3d7ad69d
Arnab Sural <arnabsural1890@gmail.com>
2026-06-13 12:49:16 +0530

    feat: initialize Express server with CORS and JSON middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.get("/", (req, res) => {
     res.send("CollegeHUB Backend Running 🚀");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
