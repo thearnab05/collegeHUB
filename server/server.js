0d0519f7c4c77a26aed1b32533bb949246fca755
Arnab Sural <arnabsural1890@gmail.com>
2026-06-14 23:50:28 +0530

    feat: initialize express server with mongodb connection and basic routing

--- server/server.js
+++ server/server.js
@@ -4,8 +4,16 @@ require("dotenv").config();
 
 const app = express();
 
+const mongoose = require("mongoose");
+
 app.use(cors());
 app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
     res.send("CollegeHUB Backend Running 🚀");
